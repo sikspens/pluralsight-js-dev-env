1e22e34f5af0c7189bcb193101088f87dc23900d
[sikspens] <[swantube3@gmail.com]>
2018-04-05 18:53:39 +0200

    second DEMO commit

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -6,7 +6,7 @@ var port = 3000;
 var app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '..src/index.html'));
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err) {
@@ -15,4 +15,4 @@ app.listen(port, function(err) {
     } else {
         open('http://localhost:' + port);
     }
-})
+});
